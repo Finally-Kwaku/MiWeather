088f390dc107016d281716ac06fdf6ecf3459f85
Finally-Kwaku <kwakunagyamfi@gmail.com>
2023-12-28 23:33:02 +0000

    Added weather description and integrated weather icons using dummy components

--- miweather/src/components/current-weather/current-weather.js
+++ miweather/src/components/current-weather/current-weather.js
@@ -4,8 +4,10 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-	<p className="city">Accra</p>
-	<p className="weather-description">Sunny</p>
+	<div>
+	  <p className="city">Accra</p>
+	  <p className="weather-description">Sunny</p>
+	</div>
 	<img alt="weather" className="weather-icon" src="icons/01d.png"/>
       </div>
     </div>
