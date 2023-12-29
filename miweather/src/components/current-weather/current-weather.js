dccd77339abf811b90ce81243435d0b7144855b8
Finally-Kwaku <kwakunagyamfi@gmail.com>
2023-12-29 21:26:15 +0000

    Added more parameters to current weather and tested them using dummy values

--- miweather/src/components/current-weather/current-weather.js
+++ miweather/src/components/current-weather/current-weather.js
@@ -10,6 +10,30 @@ const CurrentWeather = () => {
 	</div>
 	<img alt="weather" className="weather-icon" src="icons/01d.png"/>
       </div>
+      <div className="bottom">
+	<p className="temperature">24°C</p>
+	<div className="details">
+	    <div className="parameter-row">
+	         <span className="parameter-details">Details</span>
+	    </div>
+	    <div className="parameter-row">
+                 <span className="parameter-details">feels like</span>
+	         <span className="parameter-value">26°C</span>
+            </div>
+	    <div className="parameter-row">
+                 <span className="parameter-details">Wind</span>
+                 <span className="parameter-value">5 m/s</span>
+            </div>
+	    <div className="parameter-row">
+                 <span className="parameter-details">Humidity</span>
+                 <span className="parameter-value">20%</span>
+            </div>
+	    <div className="parameter-row">
+                 <span className="parameter-details">Pressure</span>
+                 <span className="parameter-value">20 hPa</span>
+            </div>
+	</div>
+      </div>
     </div>
   );
 }
