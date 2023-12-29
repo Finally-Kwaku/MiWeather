dbee4851b7dbe3a9971d67fd82b99f091cbe27e1
Finally-Kwaku <kwakunagyamfi@gmail.com>
2023-12-29 22:44:36 +0000

    Removed dummy data from current weather component

--- miweather/src/components/current-weather/current-weather.js
+++ miweather/src/components/current-weather/current-weather.js
@@ -1,37 +1,41 @@
 import "./current-weather.css"
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
   return (
     <div className="weather">
       <div className="top">
 	<div>
-	  <p className="city">Accra</p>
-	  <p className="weather-description">Sunny</p>
+	  <p className="city">{data.city}</p>
+	  <p className="weather-description">{data.weather[0].description}</p>
 	</div>
-	<img alt="weather" className="weather-icon" src="icons/01d.png"/>
+	<img
+          alt="weather"
+          className="weather-icon"
+          src={`icons/${data.weather[0].icon}.png`}
+        />
       </div>
       <div className="bottom">
-	<p className="temperature">24°C</p>
+	<p className="temperature">{Math.round(data.main.temp)}°C</p>
 	<div className="details">
-	    <div className="parameter-row">
-	         <span className="parameter-details">Details</span>
-	    </div>
-	    <div className="parameter-row">
-                 <span className="parameter-details">feels like</span>
-	         <span className="parameter-value">26°C</span>
-            </div>
-	    <div className="parameter-row">
-                 <span className="parameter-details">Wind</span>
-                 <span className="parameter-value">5 m/s</span>
-            </div>
-	    <div className="parameter-row">
-                 <span className="parameter-details">Humidity</span>
-                 <span className="parameter-value">20%</span>
-            </div>
-	    <div className="parameter-row">
-                 <span className="parameter-details">Pressure</span>
-                 <span className="parameter-value">20 hPa</span>
-            </div>
+	  <div className="parameter-row">
+	    <span className="parameter-details">Details</span>
+	  </div>
+	  <div className="parameter-row">
+            <span className="parameter-details">feels like</span>
+	    <span className="parameter-value">{Math.round(data.main.feels_like)}</span>
+          </div>
+	  <div className="parameter-row">
+            <span className="parameter-details">Wind</span>
+            <span className="parameter-value">{data.wind.speed} m/s</span>
+          </div>
+	  <div className="parameter-row">
+            <span className="parameter-details">Humidity</span>
+            <span className="parameter-value">{data.main.humidity}%</span>
+          </div>
+	  <div className="parameter-row">
+            <span className="parameter-details">Pressure</span>
+            <span className="parameter-value">{data.main.pressure} hPa</span>
+          </div>
 	</div>
       </div>
     </div>
