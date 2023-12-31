662e3bbbd88e27ae640e7bccb78d88b1a5a5acf3
Finally-Kwaku <kwakunagyamfi@gmail.com>
2023-12-31 15:01:59 +0000

    Imported forecast.css to forecast.js

--- miweather/src/components/forecast/forecast.js
+++ miweather/src/components/forecast/forecast.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel } from "react-accessible-accordion";
+import "./forecast.css";
 
 
 const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
